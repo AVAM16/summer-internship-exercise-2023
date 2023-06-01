90ee76fff0649aa97fb546f553c7127b333ee8bc
Artur <arturvasco.martins@gmail.com>
2023-06-01 17:33:05 +0100

    remove useless imports

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,7 +1,5 @@
 package com.premiumminds.internship.snail;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
